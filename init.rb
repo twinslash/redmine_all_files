cfae694a4df2168e2c07ef24d28b6ca6ff6e2a2f
Dmitry Kovalenok <dzm.kov@gmail.com>
2013-02-22 12:25:48 +0300

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_all_files do
+  name 'Redmine All Files plugin'
+  author 'Dmitry Kovalenok'
+  description 'Plugin for Redmine to view all attachments related to current project'
+  version '0.0.1'
+  url 'https://github.com/twinslash/redmine_all_files'
+  author_url 'https://github.com/twinslash'
+end
