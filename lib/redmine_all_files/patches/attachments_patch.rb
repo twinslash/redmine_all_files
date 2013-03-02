f899edf096b049f1148f5e239c25a590efa2ae42
Dmitry Kovalenok <dzm.kov@gmail.com>
2013-03-02 22:49:02 +0300

    Refactor code

--- lib/redmine_all_files/patches/attachments_patch.rb
+++ lib/redmine_all_files/patches/attachments_patch.rb
@@ -9,6 +9,8 @@ module RedmineAllFiles
         def search_attachments_for_project id, tokens = [], options = {}
           return [] if options[:scope].blank?
 
+          containers = options[:scope].map { |container| container.singularize.camelize }
+
           token_clauses = tokens.map do |token|
             "(LOWER(a.`filename`) LIKE %{token} #{ 'OR LOWER(a.`description`) LIKE %{token}' unless options[:titles_only] })" % { token: sanitize("%#{token.downcase}%") }
           end
@@ -16,35 +18,26 @@ module RedmineAllFiles
           statement = self.sanitize true if statement.blank?
 
           find_by_sql <<-SQL
-            SELECT
-            #{ "d.`title` AS document_title," if options[:scope].include? 'documents' }
-            #{ "i.`subject` AS issue_subject, t.`name` AS issue_tracker_name," if options[:scope].include? 'issues' }
-            #{ "n.`title` AS new_title," if options[:scope].include? 'news' }
-            #{ "v.`name` AS version_name," if options[:scope].include? 'versions' }
-            #{ "w.`title` AS wiki_page_title," if options[:scope].include? 'wiki_pages' }
-            a.*
-
+            SELECT d.`title` AS document_title, i.`subject` AS issue_subject, t.`name` AS issue_tracker_name,
+                   n.`title` AS new_title, v.`name` AS version_name, w.`title` AS wiki_page_title, a.*
             FROM `attachments` a
 
-            #{ "LEFT JOIN `documents` d ON d.`id` = a.`container_id` AND a.`container_type` = 'Document'" if options[:scope].include? 'documents' }
-            #{ "LEFT JOIN `issues` i ON i.`id` = a.`container_id` AND a.`container_type` = 'Issue' LEFT JOIN `trackers` t ON t.`id` = i.`tracker_id`" if options[:scope].include? 'issues' }
-            #{ "LEFT JOIN `news` n ON n.`id` = a.`container_id` AND a.`container_type` = 'News'" if options[:scope].include? 'news' }
-            #{ "LEFT JOIN `versions` v ON v.`id` = a.`container_id` AND a.`container_type` = 'Version'" if options[:scope].include? 'versions' }
-            #{ "LEFT JOIN `wiki_pages` w ON w.`id` = a.`container_id` AND a.`container_type` = 'WikiPage' LEFT JOIN `wikis` ww ON ww.`id` = w.`wiki_id`" if options[:scope].include? 'wiki_pages' }
+            LEFT JOIN `documents` d ON d.`id` = a.`container_id` AND a.`container_type` = 'Document'
+            LEFT JOIN `issues` i ON i.`id` = a.`container_id` AND a.`container_type` = 'Issue' LEFT JOIN `trackers` t ON t.`id` = i.`tracker_id`
+            LEFT JOIN `news` n ON n.`id` = a.`container_id` AND a.`container_type` = 'News'
+            LEFT JOIN `versions` v ON v.`id` = a.`container_id` AND a.`container_type` = 'Version'
+            LEFT JOIN `wiki_pages` w ON w.`id` = a.`container_id` AND a.`container_type` = 'WikiPage' LEFT JOIN `wikis` ww ON ww.`id` = w.`wiki_id`
 
 
-            WHERE
-            (
-            #{ "d.`project_id` = #{id} OR" if options[:scope].include? 'documents' }
-            #{ "i.`project_id` = #{id} OR" if options[:scope].include? 'issues' }
-            #{ "n.`project_id` = #{id} OR" if options[:scope].include? 'news' }
-            #{ "v.`project_id` = #{id} OR" if options[:scope].include? 'versions' }
-            #{ "ww.`project_id` = #{id} OR" if options[:scope].include? 'wiki_pages' }
-            #{ "a.`container_id` = #{id} AND a.`container_type` = 'Project' OR" if options[:scope].include? 'files' }
-            #{ self.sanitize false }
-            ) AND (
-            #{ statement }
-            )
+            WHERE (d.`project_id` = #{id} OR
+                   i.`project_id` = #{id} OR
+                   n.`project_id` = #{id} OR
+                   v.`project_id` = #{id} OR
+                   ww.`project_id` = #{id} OR
+                   a.`container_id` = #{id} AND a.`container_type` = 'Project'
+                  ) AND (
+                   #{ statement }
+                  ) AND a.`container_type` IN (#{ containers.map { |c| self.sanitize(c) }.join(', ') })
             ORDER BY a.`created_on`
           SQL
         end
